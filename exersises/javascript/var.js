2e1e8fbb58e53dd968f5a1ed0150b354d5298468
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-05-26 20:07:00 +0530

    learn var&datatype in js

---
+++ exersises/javascript/var.js
@@ -0,0 +1,39 @@
+var a=3;
+a=1+a;
+var b= 5;
+var c="Hello World!"
+
+// console.log(a,b,c);
+console.log(a+b);
+console.log(c);
+console.log(typeof a,typeof b, typeof c);
+console.log(a);
+
+//data type.
+
+let a1=5;
+let b1=10;
+a1=2-a1;
+
+console.log(a1);
+console.log(a1,b1);
+
+let o={
+    name:"John","project role":"manager"
+}
+console.log(o);
+o.salary="57k";
+console.log(o);
+o.salary="90k";
+console.log(o);
+
+let a2="Hello";
+let b2=5;
+let c2=10.5;
+let d2=true;
+let e2=undefined;
+let f2=null;
+
+console.log(a2,b2,c2,d2,e2,f2);
+
+console.log( typeof a2, typeof b2, typeof c2, typeof d2, typeof e2, typeof f2);
