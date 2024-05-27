1f57c1559cd8a4995fb6e5fe99d5311dbdfc69cc
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-05-27 21:07:43 +0530

    que1

---
+++ exersises/javascript/que1.js
@@ -0,0 +1,11 @@
+//question first.create a constat object ccalled "product" to store information 
+
+const product={
+               Title:"Ball pen (black)",
+               rating:4.4,
+               isDeal:true,
+               offer:"10%",
+               price:50,
+}
+console.log(product);
+console.log(product["Title"]);
