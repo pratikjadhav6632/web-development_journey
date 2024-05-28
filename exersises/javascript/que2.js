1ed2c9a154b6cee94f8e28fa6f234b37e3796023
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-05-28 14:11:23 +0530

    que2

---
+++ exersises/javascript/que2.js
@@ -0,0 +1,16 @@
+//que 2. create a const object called "profile" to store info
+
+const profile = {
+    username:"shradha_khapra",
+    isFollow:true,
+    followers: "100k",
+    posts:195,
+    bio:"Apana college | Ex-Microsoft,DRDO To educate someone is the highest privillege",
+
+};
+console.log(profile);
+
+console.log(profile.followers);
+
+profile.posts=1+profile.posts;
+console.log(profile.posts);
