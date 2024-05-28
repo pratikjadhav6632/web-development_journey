4cf7831470c59a815bc588af7a26d4a79ea6037e
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-05-28 15:07:02 +0530

    operators in js

---
+++ exersises/javascript/operators.js
@@ -0,0 +1,66 @@
+//arithmetic operators
+
+let a = 5;
+let b = 6;
+
+console.log("a=",a, "& b=",b);
+
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+
+//modulus & exponentiation
+console.log("a%b=",a%b);
+console.log("a**b=",a**b);
+
+// post increment&decrement
+console.log("a++ =",a++);
+console.log(a);
+console.log("a-- =",a--);
+console.log(b);
+
+//pre increment & decrement
+console.log("++a =",++a);
+console.log("--a =",--a);
+
+//Assignment operators
+
+c=5;
+
+c+=2;
+console.log(c);
+
+c-=2;
+console.log(c);
+
+c*=2;
+console.log(c);
+
+c%=2;
+console.log(c);
+
+c**=2;
+console.log(c);
+
+//comparison operator
+let b1=5;
+let a1=3;
+
+console.log("b1==a1",b1==a1);
+console.log("b1!=a1",b1!=a1);
+console.log("b1===a1",b1===a1);//strict it also checks data type of both var
+console.log("b1>a1",b1>a1);
+console.log("b1<a1",b1<a1);
+
+console.log("b1>=a1",b1>=a1);
+console.log("b1<=a1",b1<=a1);
+
+//logical operators
+
+let a2=3;
+let b2=6;
+
+console.log("a2<b2 && a2<b2",a2<b2 && a2<b2)
+console.log("b1==b2 ! a2==a1",b1==b2 || a1==a1);
+console.log("a2<b2 && a2<b2",a2<b2 != a2<b2)
