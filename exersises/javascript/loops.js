636a0616f8e47e6df60bdbb3b3386e43e489364b
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-05-29 21:56:33 +0530

    Create loops.js

---
+++ exersises/javascript/loops.js
@@ -0,0 +1,21 @@
+// for loops
+
+for(let i=1;i<=5;i++){
+    console.log(i);
+}
+
+for(let j=1;j<=10;j++){
+    console.log("hello World");
+}
+
+//while
+let i2=1;
+while(i2<=5){
+    console.log(i2);
+    i2++;
+}
+//do while loop
+let i3=1;
+do{
+    console.log(i3);
+}while(i3<=5);
