ff8f4ea5f9478d135aaa29ac9c2c5b8560121d7b
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-05-30 20:54:54 +0530

    string

---
+++ exersises/javascript/string.js
@@ -0,0 +1,11 @@
+
+let str="Hello World!"//string
+
+console.log(str);
+console.log(str.length);//string length
+console.log(str[0]);//string indices
+
+//template literals
+
+let str2=`hello world! ${12} `;
+console.log(str2);
