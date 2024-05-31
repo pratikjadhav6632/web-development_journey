b6ec4f7e9a2d1e16b631b4848026224f865fa1ca
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-05-31 20:36:40 +0530

    que7

---
+++ exersises/javascript/practice que/html&js/que7.js
@@ -0,0 +1,7 @@
+//que 7. prompt the user to their full name.Genarate a username for them based on the input. Start username with @,followd by their full name and ending with the fullname length.
+
+
+
+let fullName=prompt("Enter your full name without spaces");
+let username="@"+fullName+fullName.length;
+console.log(username);
