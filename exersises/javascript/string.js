5b6e3c07eb160648eeba68e971cfdfbbc64da649
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-05-31 20:07:55 +0530

    string

--- exersises/javascript/string.js
+++ exersises/javascript/string.js
@@ -9,3 +9,36 @@ console.log(str[0]);//string indices
 
 let str2=`hello world! ${12} `;
 console.log(str2);
+
+//string methods
+
+let str3 ="  Hello World!"  ;
+//upper case
+let str3u=str3.toUpperCase()
+console.log(str3u);
+//lowercase
+let str3l=str3.toLowerCase();
+console.log(str3l);
+//trim
+let str3t=str3.trim()
+console.log(str3t);
+
+//slice
+let str4="I Love JavaScript"
+let str4s=str4.slice(2,5);
+console.log(str4s);
+
+//concat
+let str4c=str3.concat(str4);
+console.log(str4c);
+
+//replace
+let str4r=str4.replace("Love","like");
+console.log(str4r);
+
+let stra=str4.replaceAll("I Love JavaScript","i like java");
+console.log(stra);
+
+//char At
+let strc=str4.charAt(0);
+console.log(strc);
