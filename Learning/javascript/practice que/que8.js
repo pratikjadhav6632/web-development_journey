be5b1f87d45e1e6b2723bbf6f8d89729638edc61
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-06-06 22:11:01 +0530

    que8

---
+++ Learning/javascript/practice que/que8.js
@@ -0,0 +1,5 @@
+/*what is the value of the age after this code runs?
+let age=23;
+age+2;
+ans => ans would be 25. but if we print or console.log(age) it shows 23 as it is,the main reason is we cant assign any other variable for answer.
+*/
