f8c53426714fcf8bbdf8fe91615303cd3b921f79
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-06-06 22:23:24 +0530

    assign-operator

---
+++ Learning/javascript/assignmentoperator.js
@@ -0,0 +1,24 @@
+let a=10;
+let b=a;
+
+console.log(b);
+
+let age=20;
+// age =age+1;
+ age +=1;
+ console.log(age);
+
+ //age = age -1;
+ age-=1;
+
+ console.log(age);
+
+ //age = age /1;
+ age/=1;
+ console.log(age);
+
+ //age = age*1;
+ age*=1;
+ console.log(age);
+
+
