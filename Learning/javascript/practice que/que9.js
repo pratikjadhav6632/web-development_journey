e01139003caead450b30e0b7a017463fb4aa49db
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-06-07 22:25:03 +0530

    que9

---
+++ Learning/javascript/practice que/que9.js
@@ -0,0 +1,8 @@
+//que 9.write is the value of each variable in each line of code.
+
+let num=5; 
+let newNum=num++; 
+console.log(newNum);//5
+console.log(num);//6
+newNum=++num;
+console.log(newNum);//7
