f44a8e80516c42a3e20f23ba002015acbfb7e431
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-06-07 22:15:35 +0530

    unary operator

---
+++ Learning/javascript/unaryoperator.js
@@ -0,0 +1,27 @@
+// pre-increment/decrement 
+let a=10;
+
+let newage=++a;
+console.log(a);
+console.log(newage);
+
+let b=20;
+let newb=--b;
+console.log(b);
+console.log(newb);
+
+//post-increment/decrement 
+
+let a2=10;
+
+
+let newa2=a2++;
+console.log(a2);
+console.log(newa2);
+
+let b2=10;
+
+let newb2=b2--;
+console.log(b2);
+console.log(newb2);
+
