9931d5914128895c2dd775f0b6b8114c511f9b0b
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-06-08 21:41:01 +0530

    que10

---
+++ Learning/javascript/practice que/que10.js
@@ -0,0 +1,12 @@
+//que 10. find the errors in the following code?
+
+//let 1age=10; //here variable start with number, we can not start variable name with number or any other special characters 
+//ans=>
+    let age1=10;//is correct format.
+
+//let ispass=True; //here value is not bollean or string if we need to convert this value in boollean or string here the coorect wat.
+//ans=>
+    let ispass=true;//this is bollean format.
+    let ispass2='True';//this is string format.
+
+//
