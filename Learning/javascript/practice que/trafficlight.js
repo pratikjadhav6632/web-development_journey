80360a4e341f83657231f5ef81e42f3aea064225
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-06-14 21:27:36 +0530

    traffic_light que

---
+++ Learning/javascript/practice que/trafficlight.js
@@ -0,0 +1,10 @@
+let traffic_light="red";
+if(traffic_light=="red"){
+    console.log("Stop");
+}
+if(traffic_light=="yellow"){
+    console.log("Go Slow");
+}
+if(traffic_light=="green"){
+    console.log("Go");
+}
