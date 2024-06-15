ed12e9c3e5a5d31b314dd68afe185df483f12cd8
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-06-15 09:57:43 +0530

    que 11

---
+++ Learning/javascript/practice que/que11.js
@@ -0,0 +1,12 @@
+let size="xl";
+
+if(size==="xl"){
+    console.log("for xl price is Rs 250");
+}else if(size==="l"){
+    console.log("for l price is RS 200 ");
+}else if(size==="M"){
+    console.log("for M price is RS 150");
+}else{
+    console.log("for S price is Rs 100")
+}
+console.log("Vaibhav jadhav");
