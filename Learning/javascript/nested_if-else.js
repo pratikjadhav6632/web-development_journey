e6e0b19305943d724ed115791effbb396043ed42
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-06-16 09:26:09 +0530

    nested if else

---
+++ Learning/javascript/nested_if-else.js
@@ -0,0 +1,14 @@
+let mark=95;
+
+if(mark>=35){
+    console.log("Pass");
+    if(mark>=80){
+        console.log("Grade is: O");
+
+    }else{
+        console.log("Grade is: A");
+    }
+}else{
+    console.log("Better luck next time");
+
+}
