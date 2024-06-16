4ad3dc8ed9ac9f146216842fe97bcffd56024ec0
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-06-16 19:32:15 +0530

    que12

---
+++ Learning/javascript/practice que/que12.js
@@ -0,0 +1,18 @@
+//que.A "GOOD STRING" is a string that start with letter 'a'& has a length>3.Write a program to find if a string is good or not.
+let str="apple";
+
+if(str[0]==="a"&& str.length>=3){
+    console.log("This is a GOOD STRING");
+}else{
+    console.log("This is a BAD STRING");
+}
+
+//predict the output of the following code:
+
+let num=12;
+
+if((num%3===0)&&((num+1==15)||(num-1==11))){
+    console.log("safe");
+}else{
+    console.log("unsafe");
+}
