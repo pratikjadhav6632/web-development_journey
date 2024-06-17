5055388ebeb1efe07690a9c5a7b503166dbabf1a
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-06-17 22:34:15 +0530

    initial commit

--- Learning/javascript/practice que/que12.js
+++ Learning/javascript/practice que/que12.js
@@ -9,10 +9,10 @@ if(str[0]==="a"&& str.length>=3){
 
 //predict the output of the following code:
 
-let num=12;
+                                                   let num=12;
 
-if((num%3===0)&&((num+1==15)||(num-1==11))){
-    console.log("safe");
-}else{
-    console.log("unsafe");
-}
+                                                   if((num%3===0)&&((num+1==15)||(num-1==11))){
+                                                     console.log("safe");
+                                                   }else{
+                                                       console.log("unsafe");
+                                                   }
--- Learning/javascript/practice que/que8.js
+++ Learning/javascript/practice que/que8.js
@@ -4,11 +4,11 @@ age+2;
 //ans => ans would be 25. but if we print or console.log(age) it shows 23 as it is,the main reason is we cant assign any other variable for answer.
 
 
-//what is the value of avg after this code runs?
+                                                    //what is the value of avg after this code runs?
 
-let hindi=80;
-let eng=90;
-let math=100;
-let avg=(hindi+eng+math)/3;
-console.log(avg);
-//ans =>90
+                                                       let hindi=80;
+                                                       let eng=90;
+                                                       let math=100;
+                                                       let avg=(hindi+eng+math)/3;
+                                                       console.log(avg);
+
