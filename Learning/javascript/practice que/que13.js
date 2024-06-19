e409cc1208b9500b1e553152930f0354a44b626c
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-06-19 08:20:18 +0530

    que 13

---
+++ Learning/javascript/practice que/que13.js
@@ -0,0 +1,28 @@
+//use switch statement to print the day of the week using a number variable 'day' with values 1 to 7.
+
+let day=1;
+
+switch(day){
+    case 1 : console.log("Monday");
+    break;
+
+    case 2 : console.log("Tuseday");
+    break;
+
+    case 3 : console.log("Wednesday");
+    break;
+
+    case 4 : console.log("Thursday");
+    break;
+
+    case 5 : console.log("Friday");
+    break;
+
+    case 6 : console.log("Saturday");
+    break;
+
+    case 7 : console.log("Sunday");
+    break;
+
+    default : console.log("invalid number");
+}
