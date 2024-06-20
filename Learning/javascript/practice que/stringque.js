5fe31fc524767de270f750613f0f38099499766e
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-06-20 22:19:17 +0530

    string que

--- Learning/javascript/practice que/stringque.js
+++ Learning/javascript/practice que/stringque.js
@@ -2,3 +2,13 @@
 let msg=" help!";
 //trim & convert it to uppercase.
 console.log(msg.trim().toUpperCase());
+
+//for the string->let name="Apna College",predict the output for following:
+   let Name="ApnaCollege";
+   console.log(Name.slice(4,9));//Colle
+   console.log(Name.indexOf("na"));//2
+   console.log(Name.replace("Apna","Our"));//OurCollege
+
+//Separate the "college" part in the above string & replace "l"with "t"in it.
+console.log(Name.slice(4));
+console.log(Name.replace("l","t"));
