0790cad6c47d7963ff24ded23e656ad471cdc248
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-06-21 22:49:29 +0530

    array

--- Learning/javascript/array.js
+++ Learning/javascript/array.js
@@ -3,5 +3,12 @@ let students=["Aman","shardha","rohit"];
 console.log(students);
 
 console.log(students[0]);
+
+
 let fruits=["mango","banana",'gava','etc'];
+console.log(fruits);
+
+fruits[0]="cherry";
+console.log(fruits);
+fruits[1]="apple";
 console.log(fruits);
