24e97672d939cd7e9170351b1e60783f0ee79242
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-06-25 20:33:03 +0530

    initial commit

---
+++ Learning/javascript/practice que/js part 1 que/que3.js
@@ -0,0 +1,30 @@
+let score = 78;
+
+if (score >= 90 && score <= 100)
+     {
+    console.log(score, "A");
+       }
+else if (score >= 70 && score <= 89)
+     {
+    console.log(score, "B");
+       }
+else if (score >= 60 && score <= 69) 
+    {
+    console.log(score, "C");
+       }
+else if (score >= 50 && score <= 59) 
+    {
+    console.log(score, "D");
+       }
+else {
+    console.log(score, "F");
+} 
+
+let month="nov-feb";
+
+if(month===`${month}`)
+{
+    console.log(`${month} winter is here`);
+}else{
+    console.log("summer is here");
+}
--- Learning/javascript/practice que/js part 1 que/que4.js
+++ Learning/javascript/practice que/js part 1 que/que4.js
@@ -1,30 +1,7 @@
-let score = 78;
-
-if (score >= 90 && score <= 100)
-     {
-    console.log(score, "A");
-       }
-else if (score >= 70 && score <= 89)
-     {
-    console.log(score, "B");
-       }
-else if (score >= 60 && score <= 69) 
-    {
-    console.log(score, "C");
-       }
-else if (score >= 50 && score <= 59) 
-    {
-    console.log(score, "D");
-       }
-else {
-    console.log(score, "F");
-} 
-
-let month="nov-feb";
-
-if(month===`${month}`)
-{
-    console.log(`${month} winter is here`);
-}else{
-    console.log("summer is here");
+//que 5. print all even numbers from 1 to 100
+let i;
+for(i=1;i<=100;i++){
+    if(i%2==0){
+        console.log("even num is:",i);
+    }
 }
