b176eb1941c6d2e8328ec39e08243d563b0fb361
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-07-11 22:50:45 +0530

    practiceque

--- Learning/javascript/practice-que/par4que/practiceque.js
+++ Learning/javascript/practice-que/par4que/practiceque.js
@@ -7,3 +7,13 @@ for(i=0;i<arr.length;i++){
     }
 }
 console.log(arr);
+
+let arr2=[1,2,3,4,5,6,2,3];
+let num2=3;
+
+for(i=0;i<arr2.length;i++){
+    if(arr2[i]==num2){
+        arr2.splice(i,1);
+    }
+}
+console.log(arr2)
