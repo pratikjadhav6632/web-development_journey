71920fb3211d32d5640a0e102ece3c990d30a0fe
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-07-12 22:16:41 +0530

    que2

--- Learning/javascript/practice-que/par4que/practiceque.js
+++ Learning/javascript/practice-que/par4que/practiceque.js
@@ -1,3 +1,7 @@
+/*Qs1. Write a JS program to delete all occurrences of element ‘num’ in a given array.
+Example : if arr = [1, 2, 3, 4, 5, 6, 2, 3] & num = 2
+Result should be arr = [1, 3, 4, 5, 6, 3]*/
+
 let arr=[1,2,3,4,5,6,2,3];
 let num=2;
 
@@ -17,3 +21,16 @@ for(i=0;i<arr2.length;i++){
     }
 }
 console.log(arr2)
+
+/*Qs2. Write a JS program to find the no of digits in a number.
+Example : if number = 287152, count = 6*/
+
+let number = 287152;
+let count=0;
+
+let copy=number;
+while(copy>0){
+    count++;
+    copy= Math.floor(copy/10);
+}
+console.log(count);
