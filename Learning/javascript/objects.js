af4012963666510cd6c0ccc878819886db9dffce
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-08-01 15:51:27 +0530

    objects

---
+++ Learning/javascript/objects.js
@@ -0,0 +1,32 @@
+let obj={
+    name:"pratik",
+    age:19,
+    city:"pune"
+};
+console.log(obj);
+console.log(obj.name);
+
+const classInfo={
+    aman:{
+        grade:"A+",
+        city:"Dehli"
+    },
+    shraddha:{
+        grade:"A",
+        city:"Pune"
+    },
+    karan:{
+        grade:"O",
+        city:"Mumbai"
+    }
+};
+
+console.log(classInfo);
+
+console.log(classInfo.aman);
+
+console.log(classInfo.shraddha.city);
+
+console.log(classInfo.karan);
+console.log(classInfo.karan.city="Pune");
+console.log(classInfo.karan);
