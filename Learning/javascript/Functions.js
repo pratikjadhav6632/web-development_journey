7d55ad65277743c4a18bd4465ffcd0a58e2b3872
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-08-10 22:52:00 +0530

    functions

---
+++ Learning/javascript/Functions.js
@@ -0,0 +1,30 @@
+
+function hello(){
+    console.log("Hello");
+}
+
+hello();
+
+function printName(){
+    console.log("Code With Beliefs");
+    console.log("Pratik Jadhav");
+}
+printName();
+
+function print1to5(){
+    for(i=1;i<5;i++){
+        console.log(i);
+    }
+}
+print1to5();
+
+
+function eligibleForVote(){
+    age=18;
+    if(age<18){
+        console.log("Not Eligible For Voting");
+    }else{
+        console.log("Eligible For Vote");
+    }
+}
+eligibleForVote();
