d1207e595847f561e6d4aad7a20e34bc8a3cc51c
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-08-20 22:06:45 +0530

    que 2 in js part 6

--- Learning/javascript/Functions.js
+++ Learning/javascript/Functions.js
@@ -44,3 +44,4 @@ function printInfo(name,age){
 
 printInfo("Karan",21);
 
+
---
+++ Learning/javascript/practice-que/js part 6 que/que2.js
@@ -0,0 +1,10 @@
+//Create function to calculate the average .
+
+function calavg(a,b,c){
+
+    let avg= (a+b+c)/3;
+
+    console.log(avg);
+}
+
+calavg(2,4,7);
