588ed7861c849b11cd789642528b811b8004c603
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-08-21 20:56:22 +0530

    que 3 in part 6

---
+++ Learning/javascript/practice-que/js part 6 que/que3.js
@@ -0,0 +1,11 @@
+//create function which accept num from user and create a table of that number.
+
+function table(n){
+    for(let i=n;i<=n*10;i+=n)
+    {
+        console.log(i);
+    }
+}
+
+table(10);
+
