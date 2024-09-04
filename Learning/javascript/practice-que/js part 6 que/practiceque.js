66be37a2627f28e59adc38062aa93316e56ebbdc
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-09-04 11:58:20 +0530

    practice que

--- Learning/javascript/practice-que/js part 6 que/practiceque.js
+++ Learning/javascript/practice-que/js part 6 que/practiceque.js
@@ -1,4 +1,20 @@
 
+/*Qs4. Write a JavaScript function to count the number of vowels in a String
+argument.*/
+
+function countvowel(str){
+    const vowels="aeiouAEIOU";
+    let count =0;
+
+    for(let char of str){
+        if(vowels.includes(char)){
+            count++;
+        }
+    }
+    return count;
+}
+const count=countvowel("I love JAVASCRIPT");
+console.log(count);
 
 /*Qs5. Write a JavaScript function to generate a random number within a range
 (start, end).*/
