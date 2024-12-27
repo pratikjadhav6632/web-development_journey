d5cede28724cb5c7020637b2d6d75dc1def92a1c
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-12-27 19:10:30 +0530

    DOM in js

--- Learning/javascript/DOM in js/script.js
+++ Learning/javascript/DOM in js/script.js
@@ -3,43 +3,48 @@
 //Selecting Elements methods
 
 // 1. getElementById :
-     
-    document.getElementById("heading");
+
+document.getElementById("heading");
 
 // 2. gerElementByClassName :
-    document.getElementsByClassName("box");
+document.getElementsByClassName("box");
 
 // 3. getElementByTagName :
-    document.getElementsByTagName('h1');
+document.getElementsByTagName('h1');
 
 //Query Selectors.
 
 // 1. By TagName:
-    document.querySelector('p');
+document.querySelector('p');
 
 // 2. By Id :
-    document.querySelector('#id');
+document.querySelector('#id');
 
 // 3.By className: 
-    document.querySelector(".className");
+document.querySelector(".className");
 
 // 4. select all:
-    document.querySelectorAll('p');
+document.querySelectorAll('p');
 
 // 5. specific element
-    document.querySelector('div a');
+document.querySelector('div a');
 
 
 //Using properties & methods
 
 // 1.innerText:
-    Obj.innertext="changes";
+Obj.innertext = "changes";
 
 // 2.textContent:
-    Obj.textContent;
+Obj.textContent;
 
 // 3.innerHTML
-    Obj.innerHTML="Hi,I am <b>Peter Parker</b>!";
+Obj.innerHTML = "Hi,I am <b>Peter Parker</b>!";
 
+
+//Manipulating Attributes
+
+Obj.getAttribute(attr); //getter
+Obj.setAttribute(attr, val); //setter
 
 //
