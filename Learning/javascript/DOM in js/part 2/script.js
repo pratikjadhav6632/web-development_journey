564d44040dc8b0224fec2599bbe6d67555a2619b
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-12-28 23:41:51 +0530

    Event listeners

--- Learning/javascript/DOM in js/part 2/script.js
+++ Learning/javascript/DOM in js/part 2/script.js
@@ -1,8 +1,7 @@
 let btns = document.querySelectorAll("button");
 
-
 for (btn of btns) {
-    btn.onclick = notify;
+    btn.onclick = notify; //onclick
     btn.onmouseenter = mouse;
 }
 
@@ -10,7 +9,26 @@ function notify() {
     alert("Button was clicked");
     console.log("Button was clicked");
 }
-
+//onmouseenter
 function mouse() {
     console.log("Mouse is over the button");
+}
+
+//Event listener
+
+//addEventListener
+let btn2 = document.getElementsByClassName('btn');
+for (i = 0; i < btn2.length; i++) {
+    btn2[i].addEventListener('click', Hello);
+}
+
+function Hello() {
+    alert("Hey hello you clicked me!");
+}
+
+//Windows.print();
+let printbtn=document.getElementById('Print');
+printbtn.onclick=printbt;
+function printbt(){
+    window.print();
 }
