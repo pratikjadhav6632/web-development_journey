1ae6e897459460294463fa953a00b05c4159731f
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-12-29 14:46:09 +0530

    this  keyword in Event Listener

--- Learning/javascript/DOM in js/part 2/script.js
+++ Learning/javascript/DOM in js/part 2/script.js
@@ -32,3 +32,20 @@ printbtn.onclick=printbt;
 function printbt(){
     window.print();
 }
+
+//this in Event Listener
+
+let p=document.querySelector('p');
+let h1=document.querySelector('h1');
+let h3=document.querySelector('h3');
+let btn00=document.getElementById('btn');
+
+
+function changecolor(){
+    console.log(this.innerText);
+    this.style.backgroundColor="pink";
+}
+p.addEventListener('click',changecolor);
+h1.addEventListener('click',changecolor);
+h3.addEventListener('click',changecolor);
+btn00.addEventListener('click',changecolor);
