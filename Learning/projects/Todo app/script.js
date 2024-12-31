0e0c70d5603c3e047649ebed488d1a1780f31e10
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2025-01-01 00:22:13 +0530

    Add initial files for Todo app project

---
+++ Learning/projects/Todo app/script.js
@@ -0,0 +1,26 @@
+let add=document.querySelector("#button");
+let inp=document.querySelector("input");
+let ul=document.querySelector("ul");
+let lis=document.querySelectorAll("li");
+
+add.addEventListener("click",function(){
+    let item=document.createElement("li");
+    item.innerText=inp.value;
+
+    let delBtn=document.createElement('button');
+    delBtn.innerText="remove ❌";
+    delBtn.classList.add('Delete');
+    delBtn.classList.add('delBtn');
+
+    item.appendChild(delBtn);
+    ul.appendChild(item);
+    inp.value="";
+});
+
+ul.addEventListener('click',function(event){
+   if(event.target.nodeName=='BUTTON'){
+        let listItem=event.target.parentElement;
+        listItem.remove();
+        console.log("task deleted successfully");
+   }
+});
