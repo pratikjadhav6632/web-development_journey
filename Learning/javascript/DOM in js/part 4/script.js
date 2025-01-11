1d31688bba85fe58d9467f72c86c815b30dd45c7
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2025-01-11 23:08:21 +0530

    add promise-based function to simulate database save operation

--- Learning/javascript/DOM in js/part 4/script.js
+++ Learning/javascript/DOM in js/part 4/script.js
@@ -14,3 +14,18 @@ function three(){
 three();
 console.log(two());
 console.log(one());
+
+//Promises
+
+function saveDB(data){
+    return new Promise((resolve,reject)=>{
+        let internetSpeed=Math.floor(Math.random()*10)+1;
+        if(internetSpeed>4){
+            resolve("Success:Data savd ");
+        }else{
+            reject("Weak network connection");
+        }
+    });
+}
+    
+console.log(saveDB("Apnna college"));
