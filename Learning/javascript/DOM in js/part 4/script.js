bc4a3069092c3953037841067f78ca35df399f9a
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2025-01-11 23:30:31 +0530

    refactor code formatting and enhance promise handling in saveDB function

--- Learning/javascript/DOM in js/part 4/script.js
+++ Learning/javascript/DOM in js/part 4/script.js
@@ -1,14 +1,14 @@
 // Call satck
-function one(){
+function one() {
     return 1;
 }
 
-function two(){
-    return (one()+one());
+function two() {
+    return (one() + one());
 }
 
-function three(){
-    let ans=two()+one();
+function three() {
+    let ans = two() + one();
     console.log(ans);
 }
 three();
@@ -17,15 +17,43 @@ console.log(one());
 
 //Promises
 
-function saveDB(data){
-    return new Promise((resolve,reject)=>{
-        let internetSpeed=Math.floor(Math.random()*10)+1;
-        if(internetSpeed>4){
+function saveDB(data) {
+    return new Promise((resolve, reject) => {
+        let internetSpeed = Math.floor(Math.random() * 10) + 1;
+        if (internetSpeed > 4) {
             resolve("Success:Data savd ");
-        }else{
+        } else {
             reject("Weak network connection");
         }
     });
 }
-    
+
 console.log(saveDB("Apnna college"));
+
+//.then()&.catch()
+saveDB("apna collge2")
+    .then(() => {
+        console.log("promise was resolved");
+    })
+    .catch(() => {
+        console.log("promise was rejected");
+    });
+
+//promise chaining
+
+saveDB("DEMO data 1")
+    .then(()=>{
+        console.log("Data 1 saved");
+        return saveDB("DEMO data 2");
+    })
+    .then(()=>{
+        console.log("Data 2 saved");
+        return saveDB("DEMO data 3");
+    })
+    .then(()=>{
+        console.log("Data 3 saved");
+    })
+    .catch(()=>{
+        console.log("Promise was rejected");
+    });
+    
