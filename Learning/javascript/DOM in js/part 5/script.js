67f155d78055cf4dc66f484f87379e83952aa916
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2025-01-21 23:00:41 +0530

    await keyword

--- Learning/javascript/DOM in js/part 5/script.js
+++ Learning/javascript/DOM in js/part 5/script.js
@@ -12,3 +12,24 @@ let hello=async ()=>{
 }
 hello();
 
+
+// await keyword.
+
+let h1=document.querySelector("h1");
+
+  function changeColor(color,delay){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            h1.style.color=color;
+            resolve("color changed");
+        },delay);
+    });
+}
+
+async function demo(){
+    await changeColor("red",1000);
+    await changeColor("blue",1000);
+    await changeColor("green",1000);
+    await changeColor("yellow",1000);
+}
+demo();
