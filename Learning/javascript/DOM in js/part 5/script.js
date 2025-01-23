c1e7e2124705bb954b6bfda639aca0689826a227
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2025-01-23 23:13:25 +0530

    Handling rejection with Await keyword

--- Learning/javascript/DOM in js/part 5/script.js
+++ Learning/javascript/DOM in js/part 5/script.js
@@ -74,3 +74,37 @@ async function addskill(){
 }
 addskill();
 demo();
+
+
+let h2=document.querySelector("h2");
+
+  function changeColor2(color,delay){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            let num=Math.floor(Math.random()*5);
+            if(num>3){
+                reject("Promise rejected");
+            }
+            h2.style.color=color;
+            resolve("color changed");
+        },delay);
+    });
+}
+
+async function demo3(){
+    
+    try{
+    await changeColor2("red",1000);
+    await changeColor2("blue",1000);
+    await changeColor2("green",1000);
+    await changeColor2("yellow",1000);
+    }catch(err){
+        console.log("Error caught");
+        console.log(err);
+    }
+
+    a=5;
+    console.log(a+3);
+
+}
+demo3();
