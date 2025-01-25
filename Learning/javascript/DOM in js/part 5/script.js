be7ff21bed576f049c3eb53ba7a928f52ecee357
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2025-01-25 14:05:07 +0530

    Status code.

--- Learning/javascript/DOM in js/part 5/script.js
+++ Learning/javascript/DOM in js/part 5/script.js
@@ -134,3 +134,12 @@ demo3();
         DELETE //For deleting information
     */
 
+//Status code.
+
+    /*
+    200 //Ok
+    400 //Bad request
+    404 //Not found
+    500 //Internal server error
+    */
+   
