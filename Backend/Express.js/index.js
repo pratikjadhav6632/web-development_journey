eb38ab311c8c2fd9e6f2c837926bba4bf8b981d7
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-02-01 23:27:06 +0530

    learn about res.send() function

--- Backend/Express.js/index.js
+++ Backend/Express.js/index.js
@@ -10,4 +10,11 @@ app.listen(port,()=>{
 
 app.use((req,res)=>{
     console.log("Request recived");
+    //res.send("This is a basic response");
+    /*res.send({
+        name:"Apple",
+        type:"Fruit"
+    });*/
+    let code="<h1>Fruits</h1><ul><li>Apple</li><li>Mango</li></ul>"
+    res.send(code);
 });
