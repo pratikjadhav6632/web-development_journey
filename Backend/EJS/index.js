4bc41ab0dc4cd559316ed431b3bd02b4527d388b
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-02-08 00:00:35 +0530

    initial commit

--- Backend/EJS/index.js
+++ Backend/EJS/index.js
@@ -2,7 +2,11 @@ const express=require("express");
 const app=express();
 
 const port=8000;
+
 app.set("view engine","ejs");
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
+});
 app.listen(port,()=>{
     console.log(`Listening Port ${port}`);
 });
