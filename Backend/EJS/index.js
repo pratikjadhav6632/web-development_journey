26c78aae5f6a6e863f5a9448061bc80d0e69c4a5
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-02-09 23:11:57 +0530

    upadte file add some comments for better understandment

--- Backend/EJS/index.js
+++ Backend/EJS/index.js
@@ -1,15 +1,16 @@
-const express = require("express");
+const express = require("express"); //require Express.js
 const app = express();
-const path=require("path");
-const port = 8000;
+const path=require("path"); //require path
+const port = 8000; //Assign port no
 
+//EJS view Directory
 app.set("views",path.join(__dirname,"/views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) =>{
-    res.render("home");
+    res.render("home"); //Display EJS file on web
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`); //Checking port work or not
 });
