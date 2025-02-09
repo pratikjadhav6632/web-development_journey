59335489b2b65bf2d2a35c573bbbc69291ce60db
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-02-09 23:04:35 +0530

    learn about view directory (path)

--- Backend/EJS/index.js
+++ Backend/EJS/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-
+const path=require("path");
 const port = 8000;
 
+app.set("views",path.join(__dirname,"/views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) =>{
