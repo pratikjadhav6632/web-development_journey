97cf4f509e57e394c26d6697764987c24b99b729
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-02-10 22:59:54 +0530

    pasing data from Db

--- Backend/EJS/index.js
+++ Backend/EJS/index.js
@@ -11,6 +11,12 @@ app.get("/", (req, res) =>{
     res.render("home"); //Display EJS file on web
 });
 
+//passing data from server or database
+app.get("/rollDice",(req,res)=>{
+    let diceVal=Math.floor(Math.random()*6)+1;
+    res.render("rollDice",{diceVal});
+})
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`); //Checking port work or not
 });
