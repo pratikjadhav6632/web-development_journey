62bc41ebd0bb00cd1a5f11146f5b2ef4c92fbd14
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-02-15 22:35:33 +0530

    learn about  how to Handle post request

--- miscellaneous/backend/index.js
+++ miscellaneous/backend/index.js
@@ -3,6 +3,9 @@ const app=express();
 
 const port=8080;
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
 app.listen(port,()=>{
     console.log(`Listening port ${port}`);
 });
@@ -13,6 +16,7 @@ app.get("/register",(req,res)=>{
 });
 
 app.post("/register",(req,res)=>{
-    let {user,password}=req.query;
+    console.log(req.body);
+    let {user,password}=req.body;
     res.send(`Standard post method,Welcome ${user}`);
 });
