d5cd2bf9bf9df6a7fbe3067bb43d1fd34f9842fb
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-02-17 14:20:48 +0530

    learn about new operator and also constructor

--- miscellaneous/frontend/app.js
+++ miscellaneous/frontend/app.js
@@ -16,8 +16,14 @@ console.log(p1.talk());
 //constructor-it doesn't return anything & start with capital letters
 
 function Person(name,age){
-    this.name;
-    this.age;
+    this.name=name;
+    this.age=age;
+}
+
+Person.prototype.talk=function(){
+    console.log(`hey i am ${this.name}`);
 };
 
 let P1=new Person("raj",32);
+let P2=new Person("keshav",52);
+console.log(P1.talk===P2.talk);
