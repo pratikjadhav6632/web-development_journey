73da99eea980baac13a557a9dcc8eed057d06f9f
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-02-19 15:53:21 +0530

    refactor: improve code formatting and add URL-encoded body parsing

--- REST_Api/index.js
+++ REST_Api/index.js
@@ -1,18 +1,19 @@
-const express=require("express");
-const app=express();
-const port=8080;
-const path=require("path");
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
 
 
-app.set("views",path.join(__dirname,"/views"))
-app.set("views engine","ejs");
+app.set("views", path.join(__dirname, "/views"))
+app.set("views engine", "ejs");
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Listening port ${port}`);
 });
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Server is working");
 })
