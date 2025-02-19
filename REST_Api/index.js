15cd0a45305204da26958ff417f92b964a48fd82
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-02-19 15:52:05 +0530

    set up view engine and static file serving in Express app

--- REST_Api/index.js
+++ REST_Api/index.js
@@ -3,6 +3,12 @@ const app=express();
 const port=8080;
 const path=require("path");
 
+
+app.set("views",path.join(__dirname,"/views"))
+app.set("views engine","ejs");
+
+app.use(express.static(path.join(__dirname,"public")));
+
 app.listen(port,()=>{
     console.log(`Listening port ${port}`);
 });
