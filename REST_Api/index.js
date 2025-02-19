bcad914166532326f83f4a71bf8a922f780b7711
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-02-19 20:10:46 +0530

    chore: add initial configuration files and styles for the REST API

--- REST_Api/index.js
+++ REST_Api/index.js
@@ -1,19 +1,44 @@
+// Import the express module
 const express = require("express");
+// Create an instance of express
 const app = express();
+// Define the port number
 const port = 8080;
+// Import the path module
 const path = require("path");
 
-
-app.set("views", path.join(__dirname, "/views"))
-app.set("views engine", "ejs");
-
-app.use(express.static(path.join(__dirname, "public")));
+// Middleware to parse URL-encoded data
 app.use(express.urlencoded({ extended: true }));
 
+// Set the views directory
+app.set("views", path.join(__dirname, "/views"));
+// Set the view engine to ejs
+app.set("view engine", "ejs");
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
+//adding demo data 
+let posts=[
+    {
+        username:"ajay",
+        content:"I am Ajay i Got new Opportunity at MNC"
+    },
+    {
+        username:"keshav",
+        content:"Hard work pay off.."
+    },
+    {
+        username:"Riya",
+        content:"Finally i got an internship.."
+    }
+];
+// Start the server and listen on the defined port
 app.listen(port, () => {
     console.log(`Listening port ${port}`);
 });
 
-app.get("/", (req, res) => {
-    res.send("Server is working");
-})
+// Define a route for the root URL
+app.get("/posts", (req, res) => {
+    res.render("index.ejs",{posts});
+});
