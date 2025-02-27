9950b1b292dcbf6013dbd04fc4cc1836fa662404
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-02-27 13:54:15 +0530

    establish database connection and query temp table; refactor getRandomUser function

--- Database/SQL-node/index.js
+++ Database/SQL-node/index.js
@@ -1,11 +1,28 @@
-const {faker}=require('@faker-js/faker');
+const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
 
-let getRandomUser=()=>{
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database:'delta_app',
+    password:'Pratik#07'
+});
+try {
+    connection.query("SELECT * FROM temp", (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    });
+} catch (err) {
+    console.log(err);
+}
+connection.end();
+let getRandomUser = () => {
     return {
-        user_id:faker.string.uuid(),
-       username: faker.internet.username(),
-        email:faker.internet.email(),
-        password:faker.internet.password()
+        user_id: faker.string.uuid(),
+        username: faker.internet.username(),
+        email: faker.internet.email(),
+        password: faker.internet.password()
     };
 };
 
