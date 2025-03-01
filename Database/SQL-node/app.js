f3afb4c5b5a50ca5f8458dfa7c8eab0fa791f8d5
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-01 11:40:37 +0530

    add method-override dependency and create necessary configuration files; include initial views for user management

--- Database/SQL-node/app.js
+++ Database/SQL-node/app.js
@@ -1,12 +1,17 @@
 const express=require("express");
+const { readlinkSync } = require("fs");
 const app=express();
 const Port=8080;
 const mysql=require("mysql2");
 const path=require("path");
+const methodOverride=require("method-override");
 
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 
+app.use(express.static(path.join(__dirname,"/public/css")));
 
 // create the connection to database
 const connection = mysql.createConnection({
@@ -31,6 +36,35 @@ app.get("/",(req,res)=>{
     }
 
 })
+
+app.get("/user",(req,res)=>{
+    let q=`SELECT * FROM user`;
+    try{
+        connection.query(q,(err,result)=>{
+            // res.send(result);
+            let users=result;
+            res.render("user.ejs",{users});
+        })
+    }catch(err){
+        console.log(err);
+    }
+})
+app.get("/user/:id/edit",(req,res)=>{
+    let {id}=req.params;
+    let q=`SELECT * FROM user WHERE id='${id}'`
+    try{
+        connection.query(q,(err,result)=>{
+            let user=result[0];
+            res.render("edit.ejs",{user});
+        })
+    }catch(err){
+        console.log(err);
+    }
+})
+
+app.patch("/user/:id",(req,res)=>{
+    res.redirect("/user");
+});
 app.listen(Port,(req,res)=>{
     console.log(`Listening port ${Port}`);
 });
