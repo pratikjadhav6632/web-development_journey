ffc8056c3c0ea5710a8ee71a56f1a57b3ff430ab
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-04 22:47:58 +0530

    refactor: improve formatting and add user instance save logic in MongoDB connection

--- Database/Mongo-db/index.js
+++ Database/Mongo-db/index.js
@@ -8,20 +8,28 @@ main()
         console.log(err);
     })
 
-async function main(){
-   await  mongoose.connect("mongodb://127.0.0.1:27017/test");
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
 
-const userSchema=new mongoose.Schema({
-    name:String,
-    email:String,
-    age:Number
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number
 })
 
-const User=mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
-const user1=new User({
-    name:'ajay',
-    email:'ajay@yahoo.in',
-    age:42
+const user1 = new User({
+    name: 'ajay',
+    email: 'ajay@yahoo.in',
+    age: 42
 });
+
+user1.save()
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
