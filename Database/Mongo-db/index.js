6c0d51acdd5bed5ab7a1cb088de1db1ebb544442
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-04 22:22:12 +0530

    add user schema for MongoDB

--- Database/Mongo-db/index.js
+++ Database/Mongo-db/index.js
@@ -11,3 +11,9 @@ main()
 async function main(){
    await  mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
+
+const userSchema=mongoose.Schema({
+    name:String,
+    email:String,
+    age:Number
+})
