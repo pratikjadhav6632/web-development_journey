1870c3a8d93a9c12b6a6b025715febc4e36d8dd4
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-06 05:49:14 +0530

    Update user data in two different way 1.updateOne() and 2.updateMany()

--- Database/Mongo-db/index.js
+++ Database/Mongo-db/index.js
@@ -20,47 +20,59 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-const user1 = new User({
-    name: 'ajay',
-    email: 'ajay@yahoo.in',
-    age: 42
-});
+// const user1 = new User({
+//     name: 'ajay',
+//     email: 'ajay@yahoo.in',
+//     age: 42
+// });
 
-user1.save()
-    .then((res) => {
-        console.log(res);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+// user1.save()
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
 
-User.insertMany([
-    {name:'kiran',email:'kiran@gmail.com',age:20},
-    {name:'shweta',email:'shweta@yahoo.in',age:24}
-]).then((res)=>{
+// User.insertMany([
+//     {name:'kiran',email:'kiran@gmail.com',age:20},
+//     {name:'shweta',email:'shweta@yahoo.in',age:24}
+// ]).then((res)=>{
+//     console.log(res);
+// });
+
+
+// User.find({}).then((res)=>{
+//     console.log(res)
+
+// }).catch((err)=>{
+//     console.log(err);
+// });
+
+
+// User.find({age:{$lte:20}}).then((res)=>{
+//     console.log(res);
+
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+// User.findOne({age:{$gte:24}}).then((res)=>{
+//     console.log(res);
+// })
+
+// User.findById("67c735b72cadeda31aed257a").then((res)=>{
+//     console.log(res.name);
+// })
+
+User.updateOne({name:'ajay'},{age:50}).then((res)=>{
     console.log(res);
-});
-
-
-User.find({}).then((res)=>{
-    console.log(res)
-
 }).catch((err)=>{
     console.log(err);
 });
 
-
-User.find({age:{$lte:20}}).then((res)=>{
+User.updateMany({age:24},{age:25}).then((res)=>{
     console.log(res);
-
 }).catch((err)=>{
     console.log(err);
-})
-
-User.findOne({age:{$gte:24}}).then((res)=>{
-    console.log(res);
-})
-
-User.findById("67c735b72cadeda31aed257a").then((res)=>{
-    console.log(res.name);
 })
