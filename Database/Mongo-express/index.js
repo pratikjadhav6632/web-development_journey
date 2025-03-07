62bead3a2d9517d8ad26204bad52de18f7fdb55d
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-07 21:00:28 +0530

    settting up ejs enviroment

--- Database/Mongo-express/index.js
+++ Database/Mongo-express/index.js
@@ -1,7 +1,14 @@
 const express=require("express");
 const app=express();
+const path=require("path");
+
+app.set("views", path.join(__dirname,"views"));
+app.set("view engine","ejs");
 
 
+app.get("/work",(req,res)=>{
+    res.render("index.ejs");
+})
 app.get("/",(req,res)=>{
     res.send("root is working");
 })
