5d4eb3b3aba92133cb5c0d12893d904efe5b2312
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-08 00:04:20 +0530

    setting up Mongo-db enviroment

--- Database/Mongo-express/index.js
+++ Database/Mongo-express/index.js
@@ -1,18 +1,28 @@
-const express=require("express");
-const app=express();
-const path=require("path");
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
 
-app.set("views", path.join(__dirname,"views"));
-app.set("view engine","ejs");
+main().then((res) => {
+    console.log("Connection succesfully..");
+}).catch((err) => {
+    console.log(err);
+})
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/whatschat");
+}
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 
-app.get("/work",(req,res)=>{
+app.get("/work", (req, res) => {
     res.render("index.ejs");
 })
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("root is working");
 })
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("Server is Listening port 8080");
 });
