05d0ad3f852e7bb71e3974317cc23fd9efdf9146
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-07 20:19:34 +0530

    feat: update price validation message in book schema

--- Database/Mongo-db/books.js
+++ Database/Mongo-db/books.js
@@ -21,7 +21,7 @@ const bookSchema=mongoose.Schema({
     },
     price:{
         type:Number,
-        min:1,
+        min:[1,'Price is too Low for amazon listing...'],
     },
     discount:{
         type:Number,
@@ -54,14 +54,20 @@ const Book=mongoose.model("Book",bookSchema);
 //     console.log(err);
 // });
 
-const book2=new Book({
-    title:"marvel comics",
-    price:100,
-    categoary:"fiction",
+// const book2=new Book({
+//     title:"marvel comics",
+//     price:100,
+//     categoary:"fiction",
 
-});
-book2.save().then((res)=>{
+// });
+// book2.save().then((res)=>{
+//     console.log(res);
+// }).catch((err)=>{
+//     console.log(err);
+// });
+
+Book.findByIdAndUpdate("67c96bda9604af11c2304377",{price:-200},{runValidators:true}).then((res)=>{
     console.log(res);
 }).catch((err)=>{
-    console.log(err);
+    console.log(err.errors.price.properties.message);
 });
