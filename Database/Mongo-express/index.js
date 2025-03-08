b9302202bfe97270168add99e7e4f969cb76e3c1
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-08 11:04:09 +0530

    Create model folder and define schema

--- Database/Mongo-express/index.js
+++ Database/Mongo-express/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const Chat=require("./model/chat.js")
 
 main().then((res) => {
     console.log("Connection succesfully..");
@@ -11,6 +12,17 @@ main().then((res) => {
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/whatschat");
 }
+
+let chat1=new Chat({
+    from:"ajay",
+    to:"kiran",
+    message:"hey how are you",
+    created_at:new Date()
+});
+
+chat1.save().then((res)=>{
+    console.log(res);
+})
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
