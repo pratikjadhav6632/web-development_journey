8d270c45ec78aaa6c4da61188933eed0476ef687
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-08 11:19:08 +0530

    initialize MongoDB connection and seed chat data

---
+++ Database/Mongo-express/init.js
@@ -0,0 +1,41 @@
+const mongoose=require("mongoose");
+const Chat=require("./model/chat.js");
+
+main().then((res)=>{
+    console.log("Connection successfully...");
+}).catch((err)=>{
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/whatschat");
+}
+
+const allChat=[
+    {
+        from:"sita",
+        to:"kalyan",
+        message:"how are you!",
+        created_at:new Date()
+    },
+
+    {
+        from:"komal",
+        to:"riya",
+        message:" I want your math notebook ",
+        created_at:new Date()
+    },
+    {
+        from:"suhil",
+        to:"kiyara",
+        message:" im busy now ",
+        created_at:new Date()
+    },
+    {
+        from:"ajay",
+        to:"vijay",
+        message:"hey can we talk now",
+        created_at:new Date()
+    },
+];
+Chat.insertMany(allChat);
