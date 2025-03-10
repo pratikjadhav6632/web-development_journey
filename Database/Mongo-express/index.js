76c79ebf7e7b3bf166c8d5b34b57716a9231adee
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-10 20:25:10 +0530

    remove MongoDB connection and chat data initialization code from index.js

--- Database/Mongo-express/index.js
+++ Database/Mongo-express/index.js
@@ -4,26 +4,6 @@ const path = require("path");
 const mongoose = require("mongoose");
 const Chat=require("./model/chat.js")
 
-main().then((res) => {
-    console.log("Connection succesfully..");
-}).catch((err) => {
-    console.log(err);
-})
-async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/whatschat");
-}
-
-let chat1=new Chat({
-    from:"ajay",
-    to:"kiran",
-    message:"hey how are you",
-    created_at:new Date()
-});
-
-chat1.save().then((res)=>{
-    console.log(res);
-})
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
