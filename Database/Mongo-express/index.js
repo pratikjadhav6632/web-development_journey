756a454e20e62587640e5245b8bdef4cc69bae05
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-10 20:42:56 +0530

    establish MongoDB connection and render chat data in new chat view

--- Database/Mongo-express/index.js
+++ Database/Mongo-express/index.js
@@ -4,6 +4,17 @@ const path = require("path");
 const mongoose = require("mongoose");
 const Chat=require("./model/chat.js")
 
+
+main().then((res)=>{
+    console.log("Connection successfully...");
+}).catch((err)=>{
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/whatschat");
+}
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -14,7 +25,7 @@ app.get("/", (req, res) => {
 //Chats Route
 app.get("/chats", async(req, res) => {
     let chats=await Chat.find();
-    res.send("work");
+    res.render("chat.ejs",{chats});
 })
 
 app.listen(8080, () => {
