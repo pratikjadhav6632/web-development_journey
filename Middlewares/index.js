b2d0cb7b2a985d3987e55574090679633ef37fb4
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-23 10:02:54 +0530

    Use next();

--- Middlewares/index.js
+++ Middlewares/index.js
@@ -5,14 +5,19 @@ app.listen("8080",()=>{
     console.log("Listening port 8080");
 })
 
-
+//using next()
+app.use((req,res,next)=>{
+    console.log("time",Date.now());
+    next();
+})
 
 //using req&res object in middleware
 app.use((req,res)=>{
     console.log("I am Another one middleware");
-    res.send("Byy..");
+   
 })
 //app.use(middleware)
-app.use(()=>{
+app.use("/",(req,res)=>{
     console.log("Hi, I am a Middleware..")
+    res.send("Byy..");
 })
