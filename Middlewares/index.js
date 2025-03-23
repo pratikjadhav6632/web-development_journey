589fd428a02f8ef3b56ea89d50498634d7533fb0
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-23 10:37:02 +0530

    Add logger middleware to capture request details and timestamp

--- Middlewares/index.js
+++ Middlewares/index.js
@@ -5,6 +5,12 @@ app.listen("8080",()=>{
     console.log("Listening port 8080");
 })
 
+//logger 
+app.use((req,res,next)=>{
+    req.time=new Date(Date.now()).toString();
+    console.log(req.method,req.hostname,req.path,req.time);
+    next();
+})
 //using next()
 app.use((req,res,next)=>{
     console.log("time",Date.now());
