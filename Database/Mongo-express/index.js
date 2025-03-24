f37c1f97759a2a5899fcd1895594a929598bb5c9
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-24 20:25:00 +0530

    Refactor chat routes to use asyncWrap for improved error handling and code consistency

--- Database/Mongo-express/index.js
+++ Database/Mongo-express/index.js
@@ -26,27 +26,31 @@ app.use(express.static(path.join(__dirname, "public/css")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+
+//Error Handler asyncWrap function
+function asyncWrap(fn){
+    return function(req,res,next){
+        fn(req,res,next).catch((err)=>{next(err)})
+    };
+};
 app.get("/", (req, res) => {
     res.send("root is working");
 })
 
 //Chats Route
-app.get("/chats", async (req, res,next) => {
-    try {
+app.get("/chats", asyncWrap(async (req, res,next) => {
+
         let chats = await Chat.find();
         res.render("chat.ejs", { chats });
-    } catch (err) {
-        next(err);
-    }
-
-})
+    
+}));
 
 //New Route
 app.get("/chats/new", (req, res) => {
     res.render("new.ejs")
 })
-app.post("/chats",async (req, res,next) => {
-    try {
+app.post("/chats",asyncWrap(async (req, res,next) => {
+  
         let { from, to, message } = req.body;
         let newChat = new Chat({
             from: from,
@@ -56,60 +60,47 @@ app.post("/chats",async (req, res,next) => {
         });
        await newChat.save();
         res.redirect("/chats");
-    }catch(err) {
-        next(err);
-    }
-});
+   
+}));
 
 //Edit route
 
-app.get("/chats/:id/edit", async (req, res,next) => {
-    try {
+app.get("/chats/:id/edit", asyncWrap(async (req, res,next) => {
+   
         let { id } = req.params;
         let chat = await Chat.findById(id);
         res.render("edit.ejs", { chat });
-    } catch (err) {
-        next(err);
-    }
-
-
-})
+  
+}));
 
 //updated route
-app.patch("/chats/:id", async (req, res,next) => {
-    try {
+app.patch("/chats/:id", asyncWrap(async (req, res,next) => {
 
         let { id } = req.params;
         let { message: newChat } = req.body;
         let chat = await Chat.findByIdAndUpdate(id, { message: newChat }, { runValidators: true, new: true });
         //console.log(chat);
         res.redirect("/chats");
-    } catch (err) {
-        next(err);
-    }
-})
+    
+}));
 
 //delete route
 
-app.delete("/chats/:id", async (req, res,next) => {
-    try {
-
+app.delete("/chats/:id", asyncWrap(async (req, res,next) => {
+ 
         let { id } = req.params;
         let delChat = await Chat.findByIdAndDelete(id);
         console.log(delChat);
         res.redirect("/chats");
-    } catch (err) {
-        next(err);
-    }
-})
+ 
+}));
 app.listen(8080, () => {
     console.log("Server is Listening port 8080");
 });
 
 //NEW Show-route
 
-app.get("/chats/:id", async (req, res, next) => {
-    try {
+app.get("/chats/:id", asyncWrap(async (req, res, next) => {
 
         let { id } = req.params;
         let chat = await Chat.findById(id);
@@ -117,11 +108,7 @@ app.get("/chats/:id", async (req, res, next) => {
             next(new ExpressError(404, "Chat not found"));
         }
         res.render("show.ejs", { chat });
-    } catch (err) {
-        next(err);
-
-    }
-})
+}));
 
 app.use((err, req, res, next) => {
     let { status=404, message="some err occured" } = err;
