361cf776c3ab63e420a5134b2f8956c3c7115dd5
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-04-08 22:32:48 +0530

    Add initial customer.js file with user schema and MongoDB connection

---
+++ Database/Relationships/customer.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose");
+const{Schema}=mongoose;
+
+main()
+    .then(() => {
+        console.log("Successfully connected..");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/relation");
+}
+
+const userSchema=new Schema({
+    username:String,
+    addresses:[
+        {
+        _id:false,
+        location:String,
+        city:String
+         },
+    ],
+});
+
+const User=mongoose.model("User",userSchema);
