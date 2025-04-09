63168f8acadef2bbf4668202cfc49b0efa7160ee
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-04-09 20:38:49 +0530

    Add order and customer schema

--- Database/Relationships/customer.js
+++ Database/Relationships/customer.js
@@ -13,22 +13,30 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/relation");
 }
 
-const userSchema=new Schema({
-    username:String,
-    addresses:[
-        {
-        _id:false,
-        location:String,
-        city:String
-         },
-    ],
-});
-
 const orderSchema=new Schema({
   item:String,
   price:Number,
 });
 
+const customerSchema=new Schema({
+    name:String,
+    orders:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:Order
+        }
+    ]
+})
 
+const Customer=mongoose.model("Customer",customerSchema);
+const Order=mongoose.model("Order",orderSchema);
 
-const User=mongoose.model("User",userSchema);
+let addOrder=async ()=>{
+    let res=await Order.insertMany([
+        {item:"Chips",price:40},
+        {item:"fruits",price:120},
+        {item:"snacks",price:60},
+   ] );
+    console.log(res);
+}
+addOrder();
