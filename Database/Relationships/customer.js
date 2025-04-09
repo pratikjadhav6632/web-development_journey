c5e1fd5d6954ed818b2fb8cdaab56a2a7ef8f6c2
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-04-09 20:46:47 +0530

    Fix reference in customerSchema and clean up addOrder function

--- Database/Relationships/customer.js
+++ Database/Relationships/customer.js
@@ -23,7 +23,7 @@ const customerSchema=new Schema({
     orders:[
         {
             type:Schema.Types.ObjectId,
-            ref:Order
+            ref:"Order"
         }
     ]
 })
@@ -31,12 +31,27 @@ const customerSchema=new Schema({
 const Customer=mongoose.model("Customer",customerSchema);
 const Order=mongoose.model("Order",orderSchema);
 
-let addOrder=async ()=>{
-    let res=await Order.insertMany([
-        {item:"Chips",price:40},
-        {item:"fruits",price:120},
-        {item:"snacks",price:60},
-   ] );
-    console.log(res);
+let addCust=async()=>{
+    let cust1=new Customer({
+        name:"Rahul K"
+    });
+    let order1=await Order.findOne({item:"Chips"});
+    let order2=await Order.findOne({item:"snacks"});
+
+    cust1.orders.push(order1);
+    cust1.orders.push(order2);
+
+    let result=await cust1.save();
+    console.log(result);
 }
-addOrder();
+addCust();
+
+// let addOrder=async ()=>{
+//     let res=await Order.insertMany([
+//         {item:"Chips",price:40},
+//         {item:"fruits",price:120},
+//         {item:"snacks",price:60},
+//    ] );
+//     console.log(res);
+// }
+// addOrder();
