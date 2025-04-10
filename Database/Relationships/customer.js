37a252912d66d5194863d0093526b5ed7965f63b
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-04-10 14:08:23 +0530

    add deletion middleware

--- Database/Relationships/customer.js
+++ Database/Relationships/customer.js
@@ -1,5 +1,5 @@
-const mongoose=require("mongoose");
-const{Schema}=mongoose;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 main()
     .then(() => {
@@ -13,23 +13,34 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/relation");
 }
 
-const orderSchema=new Schema({
-  item:String,
-  price:Number,
+const orderSchema = new Schema({
+    item: String,
+    price: Number,
 });
 
-const customerSchema=new Schema({
-    name:String,
-    orders:[
+const customerSchema = new Schema({
+    name: String,
+    orders: [
         {
-            type:Schema.Types.ObjectId,
-            ref:"Order"
+            type: Schema.Types.ObjectId,
+            ref: "Order"
         }
     ]
 })
 
-const Customer=mongoose.model("Customer",customerSchema);
-const Order=mongoose.model("Order",orderSchema);
+customerSchema.post("findOneAndDelete", async (customer) => {
+    try {
+        if (customer.orders.length) {
+            let res = await Order.deleteMany({ _id: { $in: customer.orders } });
+            console.log("he", res);
+        }
+    } catch (err) {
+        console.log("err");
+    }
+})
+
+const Customer = mongoose.model("Customer", customerSchema);
+const Order = mongoose.model("Order", orderSchema);
 
 // let addCust=async()=>{
 //     let cust1=new Customer({
@@ -46,13 +57,13 @@ const Order=mongoose.model("Order",orderSchema);
 // }
 // addCust();
 
-let addCust=async()=>{
-    let cust2=new Customer({
-        name:"Karan j"
+let addCust = async () => {
+    let cust2 = new Customer({
+        name: "Karan j"
     });
-    let newOrder=new Order({
-        item:"Pizza",
-        price:270
+    let newOrder = new Order({
+        item: "burger",
+        price: 270
     });
 
     cust2.orders.push(newOrder);
@@ -65,12 +76,13 @@ let addCust=async()=>{
 }
 // addCust();
 
-const delCust=async()=>{
-    let data= await Customer.findByIdAndDelete("67f690b9d02792c90bf63a2a")
+const delCust = async () => {
+    let data = await Customer.findByIdAndDelete("67f7832a592abb2eb7692eed")
     console.log(data);
 
-} 
+}
 delCust();
+
 // let addOrder=async ()=>{
 //     let res=await Order.insertMany([
 //         {item:"Chips",price:40},
