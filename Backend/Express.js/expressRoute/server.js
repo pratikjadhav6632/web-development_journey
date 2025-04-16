162a8983db4c0d9cfee29a5177415a5aac5d17f3
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-04-16 23:32:27 +0530

    Implement cookie-parser with a secret key and add endpoints for signed cookie handling

--- Backend/Express.js/expressRoute/server.js
+++ Backend/Express.js/expressRoute/server.js
@@ -5,7 +5,7 @@ const User=require("./routes/user.js");
 const Post=require("./routes/post.js");
 
 //cookieParser
-app.use(cookieParser());
+app.use(cookieParser("secreteKey"));
 
 //User 
 app.use("/user",User);
@@ -13,10 +13,21 @@ app.use("/user",User);
 //Post
 app.use("/posts",Post);
 
+app.get("/getsignedcookie",(req,res)=>{
+    res.cookie("Made-in","India",{signed:true});
+    res.send("sent signed cookie")
+});
+
+app.get("/verify",(req,res)=>
+    {
+    console.log(req.signedCookies);
+    res.send("Verified");
+})
 app.get("/greet",(req,res)=>{
     let {MadeIn="Bharat"}=req.cookies;
     res.send(`Hi,${MadeIn}`);
 })
+
 app.get("/getcookies",(req,res)=>{
     res.cookie("Greet","namaste");
     res.cookie("MadeIn","India");
