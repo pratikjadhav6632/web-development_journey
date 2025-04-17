cc1567bd54b56b4714544e44ffbd8b2334f418c3
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-04-17 20:01:56 +0530

    Learn about express session

--- Backend/Express.js/expressRoute/server.js
+++ Backend/Express.js/expressRoute/server.js
@@ -3,6 +3,10 @@ const app=express();
 const cookieParser=require("cookie-parser");
 const User=require("./routes/user.js");
 const Post=require("./routes/post.js");
+const session=require("express-session");
+
+//express-session
+app.use(session({secret:"superSecret"}));
 
 //cookieParser
 app.use(cookieParser("secreteKey"));
@@ -13,26 +17,32 @@ app.use("/user",User);
 //Post
 app.use("/posts",Post);
 
-app.get("/getsignedcookie",(req,res)=>{
-    res.cookie("Made-in","India",{signed:true});
-    res.send("sent signed cookie")
+
+app.get("/test",(req,res)=>{
+    res.send("session successful..");
 });
 
-app.get("/verify",(req,res)=>
-    {
-    console.log(req.signedCookies);
-    res.send("Verified");
-})
-app.get("/greet",(req,res)=>{
-    let {MadeIn="Bharat"}=req.cookies;
-    res.send(`Hi,${MadeIn}`);
-})
 
-app.get("/getcookies",(req,res)=>{
-    res.cookie("Greet","namaste");
-    res.cookie("MadeIn","India");
-    res.send("Cookies Sent successfully");
-})
+// app.get("/getsignedcookie",(req,res)=>{
+//     res.cookie("Made-in","India",{signed:true});
+//     res.send("sent signed cookie")
+// });
+
+// app.get("/verify",(req,res)=>
+//     {
+//     console.log(req.signedCookies);
+//     res.send("Verified");
+// })
+// app.get("/greet",(req,res)=>{
+//     let {MadeIn="Bharat"}=req.cookies;
+//     res.send(`Hi,${MadeIn}`);
+// })
+
+// app.get("/getcookies",(req,res)=>{
+//     res.cookie("Greet","namaste");
+//     res.cookie("MadeIn","India");
+//     res.send("Cookies Sent successfully");
+// })
 
 app.listen(3000,()=>{
     console.log("Listening port 3000");
