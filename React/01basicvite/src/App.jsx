9b3a0e93d4a9a9a1fca99f0cd127637e8ec7d104
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-05-27 00:33:10 +0530

    Refactor App and main components to improve structure and readability

--- React/01basicvite/src/App.jsx
+++ React/01basicvite/src/App.jsx
@@ -1,12 +1,15 @@
 
 import Chai from "./Chai" //import Chai component  (name should always start with Cap letter)
 function App() { 
+  const username="Pratik jadhav"
   return (
     <>
   <div class="demo">
     <Chai/>
-  <i>Pratik jadhav </i>
 
+  </div>
+  <div className="hero">
+    <i>hello {username}</i>
   </div>
     </>
   )
--- React/01basicvite/src/main.jsx
+++ React/01basicvite/src/main.jsx
@@ -1,9 +1,33 @@
 import { StrictMode } from 'react' //Import react library as StrictMode insted of react
 import { createRoot } from 'react-dom/client' //Import react-dom library as createROot insted of ReactDom
 import App from './App.jsx' 
+import React from 'react'
+import Chai from './Chai.jsx'
+
+// function customReact(reactElement,container){
+  //   const domElement=document.createElement(reactElement.type);
+  //   domElement.innerHTML=reactElement.children;
+  //   for(const prop in reactElement.props){
+  //       if(prop==="children"){
+  //         domElement.setAttribute(prop,reactElement.props[prop])
+  //       }
+  //   }
+  //   container.appendChild(domElement);
+  //  <a href="www.google.com" target='_blank'>Visit google.com</a>
+// }
+
+
+let reactElement=React.createElement(
+ 'a',
+  {
+    href:'www.google.com',
+    target:"_blank"
+  },
+  "viit google.com"
+)
+
 
 createRoot(document.getElementById('root')).render( 
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+<App/>
 ) //Get root id from html and render App component on them 
