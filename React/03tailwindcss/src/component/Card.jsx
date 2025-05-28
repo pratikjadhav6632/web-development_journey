ccb02974cc9b08604ad08a3185d5d94b8a6ae89e
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-05-28 23:29:38 +0530

    learn some basic tailwindcss classes

--- React/03tailwindcss/src/component/Card.jsx
+++ React/03tailwindcss/src/component/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Card({btnName="visit Me",username}) {
+function Card({btnName="visit Me",username="plant"}) {
    
   return (
     <div className="max-w-xs rounded-md shadow-md bg-black text-gray-100">
@@ -8,15 +8,14 @@ function Card({btnName="visit Me",username}) {
         src="https://images.unsplash.com/photo-1746908592757-2c1d682b71db?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         // Direct image URL
         alt="Green leaves and foliage"
-        className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500"
+        className="object-cover object-center w-full rounded-t-md h-52 bg-gray-500"
       />
       <div className="flex flex-col justify-between p-6 space-y-8">
         <div className="space-y-2">
           <h2 className="text-3xl font-semibold tracking-wide">{username}</h2>
           <p className="text-gray-400">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
-            tempora ipsum soluta amet corporis accusantium aliquid consectetur
-            eaque!
+            tempora ipsum soluta amet.
           </p>
         </div>
         <button
