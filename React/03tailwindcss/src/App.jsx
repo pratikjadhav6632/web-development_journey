16b3797f9ea9df43eafc742a91bcd961bb07d0b7
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-05-28 20:51:37 +0530

    Refactor App component and add Card component for improved structure and UI

--- React/03tailwindcss/src/App.jsx
+++ React/03tailwindcss/src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./component/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    
-      <h1 className='bg-green-500 rounded-xl p-4'>Tailwind CSS</h1>
+      <h1 className="bg-green-500 rounded-xl p-4 mb-4">Tailwind CSS</h1>
+    <Card/>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
