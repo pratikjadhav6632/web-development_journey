3e9a35fddde234c69e7d6c4705d447294f5cc6c9
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-05-29 14:21:00 +0530

    Create responsive project with the help of tailwindcss and react

--- React/Projects/bgChanger/src/App.jsx
+++ React/Projects/bgChanger/src/App.jsx
@@ -1,48 +1,73 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [color,setColor] = useState("yellow")
+  const [color, setColor] = useState("gray");
 
   return (
-    <div className="w-full h-screen "
-    style={{backgroundColor:color}}
-    >
-  <div className='fixed flex flex-wrap justify-center bottom-14 inset-x-0 '>
-    <div className='flex flex-wrap justify-center gap-5 px-3 py-3 rounded-md  bg-white'>
-    <button className='px-3 py-3 w-18 rounded-md text-white' 
-    style={{backgroundColor:'red'}}
-    onClick={()=>setColor('red')}
-    > Red </button>
-     <button className=' px-3 py-3 w-18 rounded-md text-white' 
-     style={{backgroundColor:'yellow'}}
-     onClick={()=>setColor('yellow')}
-     > Yellow </button>
-      <button className=' px-3 py-3 w-18 rounded-md border-2 text-white' 
-      style={{backgroundColor:'green'}}
-      onClick={()=>setColor('green')}
-      > Green </button>
-       <button className=' px-3 py-3 w-18 rounded-md text-black' 
-       style={{backgroundColor:'white'}}
-       onClick={()=>setColor('white')}
-       > White </button>
-        <button className=' px-3 py-3 w-18 rounded-md text-white' 
-        style={{backgroundColor:'blueviolet'}}
-        onClick={()=>setColor('blueviolet')}
-        > Violet</button>
-         <button className=' px-3 py-3 w-18 rounded-md text-white' 
-         style={{backgroundColor:'orange'}}
-         onClick={()=>setColor('orange')}
-         > Orange </button>
-          <button className=' px-3 py-3 w-18 rounded-md text-white' 
-          style={{backgroundColor:'black'}}
-          onClick={()=>setColor('black')}
-          > Black </button>         
+    <div className="w-full h-screen duration-700 " style={{ backgroundColor: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-14 inset-x-0 ">
+        <div className="flex flex-wrap justify-center gap-5 px-3 py-3 rounded-md  bg-white">
+          <button
+            className="px-3 py-3 w-18 rounded-md text-white "
+            style={{ backgroundColor: "red" }}
+            onClick={() => setColor("red")}
+          >
+            {" "}
+            Red{" "}
+          </button>
+          <button
+            className=" px-3 py-3 w-18 rounded-md text-black "
+            style={{ backgroundColor: "yellow" }}
+            onClick={() => setColor("yellow")}
+          >
+            {" "}
+            Yellow{" "}
+          </button>
+          <button
+            className=" px-3 py-3 w-18 rounded-md border-2 text-white "
+            style={{ backgroundColor: "green" }}
+            onClick={() => setColor("green")}
+          >
+            {" "}
+            Green{" "}
+          </button>
+          <button
+            className=" px-3 py-3 w-18 rounded-md text-black border border-black "
+            style={{ backgroundColor: "white" }}
+            onClick={() => setColor("white")}
+          >
+            {" "}
+            White{" "}
+          </button>
+          <button
+            className=" px-3 py-3 w-18 rounded-md text-white "
+            style={{ backgroundColor: "blueviolet" }}
+            onClick={() => setColor("blueviolet")}
+          >
+            {" "}
+            Violet
+          </button>
+          <button
+            className=" px-3 py-3 w-18 rounded-md text-white "
+            style={{ backgroundColor: "orange" }}
+            onClick={() => setColor("orange")}
+          >
+            {" "}
+            Orange{" "}
+          </button>
+          <button
+            className=" px-3 py-3 w-18 rounded-md text-white "
+            style={{ backgroundColor: "black" }}
+            onClick={() => setColor("black")}
+          >
+            {" "}
+            Black{" "}
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
-    </div>
-   
-  )
+  );
 }
 
-export default App
+export default App;
