4313395ab5293b110e37a7ad5a6bdd1471b21fe6
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-05-29 12:04:08 +0530

    Refactor App component to implement color-changing buttons and update background color functionality

--- React/Projects/bgChanger/src/App.jsx
+++ React/Projects/bgChanger/src/App.jsx
@@ -2,14 +2,44 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [color,setColor] = useState("olive")
+  const [color,setColor] = useState("yellow")
 
   return (
-
-    <div className="w-full h-screen h"
+    <div className="w-full h-screen "
     style={{backgroundColor:color}}
     >
-  
+  <div className='fixed flex flex-wrap justify-center bottom-14 inset-x-0 '>
+    <div className='flex flex-wrap justify-center gap-5 px-3 py-3 rounded-md  bg-white'>
+    <button className='px-3 py-3 w-18 rounded-md text-white' 
+    style={{backgroundColor:'red'}}
+    onClick={()=>setColor('red')}
+    > Red </button>
+     <button className=' px-3 py-3 w-18 rounded-md text-white' 
+     style={{backgroundColor:'yellow'}}
+     onClick={()=>setColor('yellow')}
+     > Yellow </button>
+      <button className=' px-3 py-3 w-18 rounded-md border-2 text-white' 
+      style={{backgroundColor:'green'}}
+      onClick={()=>setColor('green')}
+      > Green </button>
+       <button className=' px-3 py-3 w-18 rounded-md text-black' 
+       style={{backgroundColor:'white'}}
+       onClick={()=>setColor('white')}
+       > White </button>
+        <button className=' px-3 py-3 w-18 rounded-md text-white' 
+        style={{backgroundColor:'blueviolet'}}
+        onClick={()=>setColor('blueviolet')}
+        > Violet</button>
+         <button className=' px-3 py-3 w-18 rounded-md text-white' 
+         style={{backgroundColor:'orange'}}
+         onClick={()=>setColor('orange')}
+         > Orange </button>
+          <button className=' px-3 py-3 w-18 rounded-md text-white' 
+          style={{backgroundColor:'black'}}
+          onClick={()=>setColor('black')}
+          > Black </button>         
+    </div>
+  </div>
     </div>
    
   )
