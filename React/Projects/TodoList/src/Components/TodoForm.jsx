1af6e6e703fa6da4010100f4a99455b0529f838d
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-06-15 09:58:23 +0530

    implement  logic in TodoForm and item

--- React/Projects/TodoList/src/Components/TodoForm.jsx
+++ React/Projects/TodoList/src/Components/TodoForm.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { useTodo } from '../Context/TodoContext';
 function TodoForm() {
-    
-
+    const [todo,setTodo]=useState('');
+    const {addTodo}=useTodo();
+    const add=(e)=>{
+        e.preventDefault();
+        if(!todo) return
+        addTodo({todo,completed:false})
+    }
     return (
         <form  className="flex">
             <input
--- React/Projects/TodoList/src/Components/TodoItem.jsx
+++ React/Projects/TodoList/src/Components/TodoItem.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useState}from 'react'
+import {useTodo} from '../Context/TodoContext'
 
 function TodoItem({ todo }) {
-    
+
 
     return (
         <div
