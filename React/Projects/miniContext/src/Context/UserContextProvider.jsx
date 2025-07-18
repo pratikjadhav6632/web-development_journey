d655643494008c3d83250ac9ef89a18ea30a96a2
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-07-18 22:44:27 +0530

    Fix formatting and improve readability in UserContextProvider component

--- React/Projects/miniContext/src/Context/UserContextProvider.jsx
+++ React/Projects/miniContext/src/Context/UserContextProvider.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from'react';
+import React, { useState } from "react";
 
-import UserContext from './UserContext';
+import UserContext from "./UserContext";
 
-const UserContextProvider=({children})=>{
-    const [user,setUser]=useState('')
-    return(
-        <UserContext.Provider value={{user,setUser}}>
-            {children}
-        </UserContext.Provider>
-    )
-}
+const UserContextProvider = ({ children }) => {
+  const [user, setUser] = useState("");
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
 
 export default UserContextProvider;
