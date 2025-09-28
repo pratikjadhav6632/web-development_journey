c06738a6434f5d7fb10b20be007f36e5a85c64c5
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-09-28 10:38:25 +0530

    Practice about event listeners in react

--- React/Frontend_Domination/Reacting/src/App.jsx
+++ React/Frontend_Domination/Reacting/src/App.jsx
@@ -1,9 +1,13 @@
 import react from "react";
 import Card from "./component/Card";
+import Song from "./component/Song"
 import "./index.css";
 function App() {
   return(
+    <>
     <Card/>
+    <Song/>
+    </>
   )
 }
 
---
+++ React/Frontend_Domination/Reacting/src/component/Song.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Song() {
+    const data=[
+        {name:"Paradox",desciption:"lorem ispusim dome thi sdhadg jsadbod iadas dn sdk"},
+        {name:"krishna",desciption:"lorem ispusim dome thi sdhadg jsadbod iadas dn sdk"},
+        {name:"ed shareen",desciption:"lorem ispusim dome thi sdhadg jsadbod iadas dn sdk"},
+
+    ]
+  return (
+    <div className='w-full h-screen flex flex-col justify-center gap-7 items-center bg-zinc-300'>
+     {data.map((item,index)=>(
+         <div key={index} className='w-60 bg-zinc-100 rounded-md px-4 py-4'>
+                <div className='font-semibold '>
+                    <h1>{item.name}</h1>
+                </div>
+                <div className='text-xs '>
+                    <p>{item.desciption}</p>
+                </div>
+                <div onClick={()=>{alert("Downloading started...")}} className='mt-3 py-1 px-2 bg-blue-600 rounded text-center'>
+                    <button className='  text-sm text-zinc-100' >Download now</button>
+                </div>
+        </div>
+     ))}
+    </div>
+  );
+}
+
+export default Song;
