6109ca1d97f0a87d050657fd19649be4f18802e8
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-09-29 22:52:52 +0530

    Add number deletion functionality and display in State componenttering UseState

--- React/Frontend_Domination/Reacting/src/component/State.jsx
+++ React/Frontend_Domination/Reacting/src/component/State.jsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 
 function State(){
     const [val,setVal]=useState({name:"pratik",isBanned:false})
+    const [num,setNum]=useState([1,2,3,4,5,6]);
+    const delete_num=()=>{
+       setNum(()=>num.filter((item,index)=>index!=num.length-1))
+    }
     return (
     <>
     <div className="w-full h-screen  bg-zinc-300">
@@ -13,7 +17,14 @@ function State(){
                     <h1>Banned:{val.isBanned.toString()}</h1>
                     <button onClick={()=>setVal({...val,isBanned:!val.isBanned})} className={`text-zinc-100 px-2 py-1 rounded-md ${val.isBanned?"bg-red-600":"bg-green-600"}`}>{val.isBanned?"Ban":"UnBan"}</button>
             </div>
+        </div> 
+     <div className="px-5 py-1 flex justify-center  bg-zinc-100">
+        <div>
+
+           {num.map(item=><h1>{item}</h1>)} 
+        <button onClick={()=>delete_num()} className="px-2 py-1 bg-red-500 text-xs rounded-full text-white">delete {num.length==0?" ":num.length}</button>
         </div>
+     </div>
      </div>
     </>       
     )
